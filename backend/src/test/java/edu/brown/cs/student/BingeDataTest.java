675e50302210dce1f472e58131a0ecbe2592ae98
Brian-han77 <86693364+Brian-han77@users.noreply.github.com>
2023-05-06 14:39:01 -0400

    Binge tests

--- backend/src/test/java/edu/brown/cs/student/BingeDataTest.java
+++ backend/src/test/java/edu/brown/cs/student/BingeDataTest.java
@@ -22,7 +22,7 @@ public class BingeDataTest {
 
 
     @Test
-    public void getTopGenresBasic2() throws IOException {
+    public void BingeDataBasicTest() throws IOException {
         MapCreator mapCreator = new MapCreator();
 
         String[][] movieData1 = {
@@ -44,9 +44,67 @@ public class BingeDataTest {
         assertEquals(bingeData.movies.get(0).title, "Valentine's Day");
 
         assertEquals(bingeData.shows.size(), 0);
-
+        
     }
     
-    
-    
+
+    @Test
+    public void BingeDataBasicTestWithShow() throws IOException {
+        MapCreator mapCreator = new MapCreator();
+
+        String[][] movieData1 = {
+            { "Valentine's Day", "2/14/21" },
+            { "Valentine's Day", "2/14/21" },
+            { "Valentine's Day", "3/14/21" },
+            { "Valentine's Day", "3/14/21" },
+            { "Valentine's Day", "3/14/21" },
+            { "Dear White People", "3/14/21" },
+            { "Squid Game", "3/14/21" },
+            { "Squid Game", "3/14/21" },
+            { "Squid Game", "3/1/21" },
+            { "Euphoria: level1", "1/21/21" },
+            { "asflnv", "3/14/21" }
+        };
+        
+        MakeBingeData bingeDataMaker = new MakeBingeData();
+        ArrayList<Map<String, ArrayList<ArrayList<String>>>> userHistoryMap = mapCreator.createWatchedMovieMap(movieData1);
+
+        JSONBingeData bingeData = bingeDataMaker.getBingeData(movieData1, userHistoryMap);
+
+        assertEquals(bingeData.date, "3/14/21");
+        assertEquals(bingeData.movies.size(), 2);
+        assertEquals(bingeData.movies.get(0).title, "asflnv");
+        assertEquals(bingeData.movies.get(1).title, "Valentine's Day");
+
+        assertEquals(bingeData.shows.size(), 2);
+        assertEquals(bingeData.shows.get(1).title, "Dear White People");
+        assertEquals(bingeData.shows.get(1).numEpWatched, 1);
+
+        assertEquals(bingeData.shows.get(0).title, "Squid Game");
+        assertEquals(bingeData.shows.get(0).numEpWatched, 2);
+        
+    }
+
+
+    @Test
+    public void BingeDataBasicTestEmpty() throws IOException {
+        MapCreator mapCreator = new MapCreator();
+
+        String[][] movieData1 = {
+            { "asflnv", "3/14/21" }
+        };
+        
+        MakeBingeData bingeDataMaker = new MakeBingeData();
+        ArrayList<Map<String, ArrayList<ArrayList<String>>>> userHistoryMap = mapCreator.createWatchedMovieMap(movieData1);
+
+        JSONBingeData bingeData = bingeDataMaker.getBingeData(movieData1, userHistoryMap);
+
+        assertEquals(bingeData.date, "3/14/21");
+        assertEquals(bingeData.movies.size(), 1);
+        assertEquals(bingeData.movies.get(0).title, "asflnv");
+
+        assertEquals(bingeData.shows.size(), 0);
+        
+    }
+
 }
