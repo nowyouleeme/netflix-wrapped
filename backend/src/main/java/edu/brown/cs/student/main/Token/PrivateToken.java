17ffb5f01b931a304cd8bc0f53b12d302fa5bf3b
Brian (do heon) <86693364+Brian-han77@users.noreply.github.com>
2023-05-07 15:05:23 -0400

    deleting token

--- backend/src/main/java/edu/brown/cs/student/main/Token/PrivateToken.java
+++
@@ -1,8 +0,0 @@
-package edu.brown.cs.student.main.Token;
-
-public class PrivateToken {
-    public String token;
-    public PrivateToken(){
-        token = "2edcbd73d186d2ddb0ab62223e88b891";
-    }
-}
